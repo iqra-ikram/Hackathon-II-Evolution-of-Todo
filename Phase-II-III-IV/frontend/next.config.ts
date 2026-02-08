ebd1abe07a7fb390239059945b0238c5ff3d31d4
Iqra Ikram <iqraikram58@gmail.com>
2026-02-09 01:27:19 +0500

    solving build issues

--- Phase-II-III-IV/frontend/next.config.ts
+++ Phase-II-III-IV/frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   /* config options here */
 };
+
+if (!process.env.VERCEL) {
+  nextConfig.output = "standalone";
+}
 
 export default nextConfig;
